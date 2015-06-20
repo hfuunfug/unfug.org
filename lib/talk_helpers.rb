67b2d80fa2996c9ab6e8ed4b2cebe590373f1e10
Matthias Beyer <mail@beyermatthias.de>
2015-06-20 17:28:18 +0200

    Add talk helper for getting future talks

--- lib/talk_helpers.rb
+++ lib/talk_helpers.rb
@@ -6,3 +6,11 @@ def past_and_next_talks items
     i.attributes[:kind] == :talk and i.attributes[:date] <= limit
   end.sort_by { |i| i.attributes[:date] }
 end
+
+def upcoming_talks items
+  limit = Date.today
+  items.select do |i|
+    i.attributes[:kind] == :talk and i.attributes[:date] > limit
+  end.sort_by { |i| i.attributes[:date] }
+end
+
