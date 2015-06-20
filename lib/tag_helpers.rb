df62ba8e6614abda352f058bf48f7491ff8a53e6
Matthias Beyer <mail@beyermatthias.de>
2015-06-20 15:42:27 +0200

    Add tag generator helper

--- lib/tag_helpers.rb
+++ lib/tag_helpers.rb
@@ -4,3 +4,10 @@
 def all_tags items
   items.map { |i| i.attributes[:tags] || [] }.flatten.uniq.sort
 end
+
+def generate_tag_pages
+  all_tags(items).each do |tag|
+    items << Nanoc::Item.new("", { :tag => tag }, "/tags/#{tag}/")
+  end
+end
+
