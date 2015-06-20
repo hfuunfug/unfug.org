cfd19b7ab4e7936386a7b29a3849ca7b07d3f595
Matthias Beyer <mail@beyermatthias.de>
2015-06-20 12:55:56 +0200

    Talk helper: sort by date

--- lib/talk_helpers.rb
+++ lib/talk_helpers.rb
@@ -4,5 +4,5 @@ def past_and_next_talks items
   limit = Date.today + 7
   items.select do |i|
     i.attributes[:kind] == :talk and i.attributes[:date] <= limit
-  end
+  end.sort_by { |i| i.attributes[:date] }
 end
