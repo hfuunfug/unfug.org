ff2924aa1d8e06237211daae03c83ce1529c5c71
Matthias Beyer <mail@beyermatthias.de>
2015-06-20 13:47:24 +0200

    Add helper to generate ical

---
+++ lib/ical_helpers.rb
@@ -0,0 +1,14 @@
+require 'icalendar'
+
+def generate_ical_items
+  ical = Icalendar::Calendar.new
+  only_talks(@items).each do |item|
+    event = Icalendar::Event.new
+    event.dtstart = item[:date]
+    event.summary = "Unfug"
+    event.description = item[:speakers].join(", ") + " - " + item[:title]
+    ical.add_event event
+  end
+
+  @items << Nanoc::Item.new(ical.to_ical, {:kind => :ical}, "/ical/")
+end
