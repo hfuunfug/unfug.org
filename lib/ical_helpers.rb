0f7514f28676ebdec3a4ed6f35afa5fbf06cb4f2
Matthias Beyer <mail@beyermatthias.de>
2015-06-20 16:24:14 +0200

    Add unfug start and endtime passing

--- lib/ical_helpers.rb
+++ lib/ical_helpers.rb
@@ -1,11 +1,12 @@
 require 'icalendar'
+require 'date'
 
-def generate_ical_items
+def generate_ical_items cfg
   ical = Icalendar::Calendar.new
   only_talks(@items).each do |item|
     event = Icalendar::Event.new
-    event.dtstart = item[:date]
-    event.dtend   = item[:date]
+    event.dtstart = item[:date].to_datetime + (cfg[:unfug_starttime] / 24.0)
+    event.dtend   = item[:date].to_datetime + (cfg[:unfug_endtime]   / 24.0)
     event.summary = "Unfug"
     event.description = item[:speakers].join(", ") + " - " + item[:title]
     ical.add_event event
