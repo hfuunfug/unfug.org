d264f440c4f5c0a47562a37d658c2fd0494d4023
Matthias Beyer <mail@beyermatthias.de>
2015-07-04 17:14:26 +0200

    Fixes: Use DateTime instances for comparing

--- lib/talk_helpers.rb
+++ lib/talk_helpers.rb
@@ -3,14 +3,14 @@ require 'date'
 def past_talks items
   limit = Date.today
   items.select do |i|
-    i.attributes[:kind] == :talk and i.attributes[:date] <= limit
-  end.sort_by { |i| i.attributes[:date] }
+    i.attributes[:kind] == :talk and i.attributes[:date].to_datetime <= limit
+  end.sort_by { |i| i.attributes[:date].to_datetime }
 end
 
 def upcoming_talks items
-  limit = Date.today
+  limit = DateTime.now
   items.select do |i|
-    i.attributes[:kind] == :talk and i.attributes[:date] > limit
-  end.sort_by { |i| i.attributes[:date] }
+    i.attributes[:kind] == :talk and i.attributes[:date].to_datetime > limit
+  end.sort_by { |i| i.attributes[:date].to_datetime }
 end
 
