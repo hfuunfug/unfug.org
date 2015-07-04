b5cc26cd2c1ecff6b9e70f8433a9a1996012eefb
Matthias Beyer <mail@beyermatthias.de>
2015-07-04 23:12:33 +0200

    Fix talk helper: Compare with DateTime object

--- lib/talk_helpers.rb
+++ lib/talk_helpers.rb
@@ -1,7 +1,7 @@
 require 'date'
 
 def past_talks items
-  limit = Date.today
+  limit = DateTime.now
   items.select do |i|
     i.attributes[:kind] == :talk and i.attributes[:date].to_datetime <= limit
   end.sort_by { |i| i.attributes[:date].to_datetime }
