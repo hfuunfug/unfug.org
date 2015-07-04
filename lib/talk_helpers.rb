4284e0a9629e900e030a4da58c55d7cd81340e4e
Matthias Beyer <mail@beyermatthias.de>
2015-07-04 23:15:06 +0200

    Talk helper: Talks should be sorted by date, but reverse

--- lib/talk_helpers.rb
+++ lib/talk_helpers.rb
@@ -4,13 +4,13 @@ def past_talks items
   limit = DateTime.now
   items.select do |i|
     i.attributes[:kind] == :talk and i.attributes[:date].to_datetime <= limit
-  end.sort_by { |i| i.attributes[:date].to_datetime }
+  end.sort_by { |i| i.attributes[:date].to_datetime }.reverse
 end
 
 def upcoming_talks items
   limit = DateTime.now
   items.select do |i|
     i.attributes[:kind] == :talk and i.attributes[:date].to_datetime > limit
-  end.sort_by { |i| i.attributes[:date].to_datetime }
+  end.sort_by { |i| i.attributes[:date].to_datetime }.reverse
 end
 
