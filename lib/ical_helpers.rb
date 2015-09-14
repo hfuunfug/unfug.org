d0dd87ae9dc6666d4fcf849782610c87e38e0024
Matthias Beyer <mail@beyermatthias.de>
2015-09-15 00:01:06 +0200

    Simplify ical generation

--- lib/ical_helpers.rb
+++ lib/ical_helpers.rb
@@ -1,6 +1,15 @@
 require 'icalendar'
 require 'date'
 
+def generate_event item, cfg
+  event = Icalendar::Event.new
+  event.dtstart = item[:date].to_datetime + (cfg[:unfug_starttime] / 24.0)
+  event.dtend   = item[:date].to_datetime + (cfg[:unfug_endtime]   / 24.0)
+  event.summary = "Unfug"
+  event.description = item[:speakers].join(", ") + " - " + item[:title]
+  event
+end
+
 #
 # Generate ical calendar for all talks
 #
@@ -10,12 +19,7 @@ require 'date'
 def generate_ical_items cfg
   ical = Icalendar::Calendar.new
   only_talks(@items).each do |item|
-    event = Icalendar::Event.new
-    event.dtstart = item[:date].to_datetime + (cfg[:unfug_starttime] / 24.0)
-    event.dtend   = item[:date].to_datetime + (cfg[:unfug_endtime]   / 24.0)
-    event.summary = "Unfug"
-    event.description = item[:speakers].join(", ") + " - " + item[:title]
-    ical.add_event event
+    ical.add_event generate_event(item, cfg)
   end
 
   @items << Nanoc::Item.new(ical.to_ical, {:kind => :ical}, "/ical/")
@@ -24,12 +28,7 @@ end
 def generate_ical_items_latest cfg
   ical = Icalendar::Calendar.new
   only_talks(@items).take(15).each do |item|
-    event = Icalendar::Event.new
-    event.dtstart = item[:date].to_datetime + (cfg[:unfug_starttime] / 24.0)
-    event.dtend   = item[:date].to_datetime + (cfg[:unfug_endtime]   / 24.0)
-    event.summary = "Unfug"
-    event.description = item[:speakers].join(", ") + " - " + item[:title]
-    ical.add_event event
+    ical.add_event generate_event(item, cfg)
   end
 
   @items << Nanoc::Item.new(ical.to_ical, {:kind => :ical}, "/ical-latest/")
