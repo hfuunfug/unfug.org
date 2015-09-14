c2f5367d158d14d85048cb4ef27280af73d234ff
Matthias Beyer <mail@beyermatthias.de>
2015-09-15 00:00:50 +0200

    Add helper for generate the latest 15 events as ical file

--- lib/ical_helpers.rb
+++ lib/ical_helpers.rb
@@ -20,3 +20,18 @@ def generate_ical_items cfg
 
   @items << Nanoc::Item.new(ical.to_ical, {:kind => :ical}, "/ical/")
 end
+
+def generate_ical_items_latest cfg
+  ical = Icalendar::Calendar.new
+  only_talks(@items).take(15).each do |item|
+    event = Icalendar::Event.new
+    event.dtstart = item[:date].to_datetime + (cfg[:unfug_starttime] / 24.0)
+    event.dtend   = item[:date].to_datetime + (cfg[:unfug_endtime]   / 24.0)
+    event.summary = "Unfug"
+    event.description = item[:speakers].join(", ") + " - " + item[:title]
+    ical.add_event event
+  end
+
+  @items << Nanoc::Item.new(ical.to_ical, {:kind => :ical}, "/ical-latest/")
+end
+
